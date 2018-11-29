c2bf570b3e10bd85b6d38dfc50bd526998245527
marco <none@none.com>
2018-11-29 22:48:05 +0000

    added more tests and some thougths

--- GOL/Universe.cs
+++ GOL/Universe.cs
@@ -1,6 +1,24 @@
-﻿namespace GOL
+﻿using System.Collections.Generic;
+
+namespace GOL
 {
-    class Universe
+    public class Universe
     {
+        private List<object> _cells;
+
+        private Universe()
+        {
+            _cells = new List<object>();
+        }
+
+        public static Universe GetEmptyUniverse() {
+            return new Universe();
+        }
+
+        public bool IsEmpty() { return _cells.Count == 0; }
+
+        public void AddCell(object cell) {
+            _cells.Add(cell);
+        }
     }
 }
