1097e1cfd618749a91c6268c84ad9cbcad2d2a3b
marco <none@none.com>
2018-12-03 21:52:22 +0000

    added cell class

---
+++ GOL/Cell.cs
@@ -0,0 +1,22 @@
+﻿namespace GOL
+{
+    class Cell
+    {
+        private bool _alive;
+        private int _numberOfNeighbours;
+        private bool _stableNeighbourhood => _numberOfNeighbours == 2 || _numberOfNeighbours == 3;
+        private bool _fertileNeighbourhood => _numberOfNeighbours == 3;
+
+        public Cell()
+        {
+
+        }
+
+        public bool AliveInNextGeneration()
+        {
+            return (_alive && _stableNeighbourhood) || _fertileNeighbourhood;
+        }
+
+
+    }
+}
