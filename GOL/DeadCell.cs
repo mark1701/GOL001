693f4519115a5390e4cd30d182cb532b983c7728
marco <none@none.com>
2018-12-03 22:11:54 +0000

    add specialised cells

---
+++ GOL/DeadCell.cs
@@ -0,0 +1,15 @@
+﻿namespace GOL
+{
+    class DeadCell
+    {
+        public DeadCell()
+        {
+
+        }
+
+        public bool ComeToLife(int numberOfNeighbours)
+        {
+            return numberOfNeighbours == 3;
+        }
+    }
+}
---
+++ GOL/LivingCell.cs
@@ -0,0 +1,15 @@
+﻿namespace GOL
+{
+    class LivingCell
+    {
+        public LivingCell()
+        {
+
+        }
+
+        public bool StaysAlive(int numberOfNeighbours)
+        {
+            return numberOfNeighbours == 2 || numberOfNeighbours == 3;
+        }
+    }
+}
