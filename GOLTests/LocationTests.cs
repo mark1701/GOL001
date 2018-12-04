7c955b952df27a29d95d32e9874e5253695eb1c8
marco <none@none.com>
2018-12-04 22:36:45 +0000

    added failing test for location

---
+++ GOLTests/LocationTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace GOLTests
+{
+    [TestFixture]
+    class LocationTests
+    {
+
+        [Test]
+        public void A_solitary_cell_has_no_neighbours() {
+
+
+
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
